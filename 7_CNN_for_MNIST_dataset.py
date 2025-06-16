a70034766a6758f137e4eac751e811791ab484c8
afiqismail01 <mabismail1@sheffield.ac.uk>
2025-06-16 23:47:29 +0100

    partial CNN

---
+++ 7_CNN_for_MNIST_dataset.py
@@ -0,0 +1,7 @@
+# Step 1: Setup
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.utils import to_categorical
